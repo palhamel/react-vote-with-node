ee6f0129c7942d53e74b24b8013974141a3e5bf2
Pål Hamel <pal.hamel@gmail.com>
2020-07-19 13:34:15 +0200

    Change use without redis, close backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+// const setupRedis = require('./setup/redis')
 const setupRouter = require("./setup/router");
 
 // Middle wares to enable cors and json body parsing
 const setupMiddleware = require("./setup/middleware");
 const setupDatabase = require("./setup/database");
+// const { request } = require("express");
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -18,8 +19,10 @@ async function start() {
 
   const db = await setupDatabase()
 
+  // const redisDb = await setupRedis()
+
   setupRouter(app, db);
-    console.log(client);
+    // console.log(data);
     // Start the server
     app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
